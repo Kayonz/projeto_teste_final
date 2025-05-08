0fb7a5b3f1852a07c6143c287bf7d8f1f8b7cec4
Kayonz <vitorlara@gmail.comgit config --global user.email vitorlara@gmail.comgit config --global user.email vitorlara@gmail.com>
2025-05-08 19:04:36 -0300

    utilizando o usestate

--- frontend-web/src/pages/DashboardPage.jsx
+++ frontend-web/src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 import Sidebar from "../components/SideBar.jsx";
+import React, { useState } from "react";
 
 const Container = styled.div`
   width: 100vw;
