da454bdab4c37593ec28fb7c4091ad5dedea5f81
Kayonz <vitorlara@gmail.comgit config --global user.email vitorlara@gmail.comgit config --global user.email vitorlara@gmail.com>
2025-05-10 16:58:49 -0300

    END TEST FOR CATEGORIES

--- backend/src/routes/categoriaRoutes.js
+++ backend/src/routes/categoriaRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getCategorias } from '../controllers/categoriaController.js';
-import { verifyToken } from '../middleware/authMiddleware.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
